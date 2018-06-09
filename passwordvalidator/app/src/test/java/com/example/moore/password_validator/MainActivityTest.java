f1d0982039dee4024cd3f4dda2f8bfd6cf6a4766
Jake Moore <jake.moorevr@gmail.com>
2018-06-09 19:52:14 -0300

    More tests

--- passwordvalidator/app/src/test/java/com/example/moore/password_validator/MainActivityTest.java
+++ passwordvalidator/app/src/test/java/com/example/moore/password_validator/MainActivityTest.java
@@ -8,9 +8,45 @@ public class MainActivityTest {
 
     @Test
     public void isLongEnough() {
+        String input = "PASSword123";
+        Boolean output;
+        Boolean expected = true;
+
+        MainActivity mainac = new MainActivity();
+        output = mainac.isLongEnough(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void isntLongEnough() {
+        String input = "word123";
+        Boolean output;
+        Boolean expected = false;
+
+        MainActivity mainac = new MainActivity();
+        output = mainac.isLongEnough(input);
+        assertEquals(expected, output);
     }
 
     @Test
     public void isntPassword() {
+        String input = "password123";
+        Boolean output;
+        Boolean expected = true;
+
+        MainActivity mainac = new MainActivity();
+        output = mainac.isntPassword(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void isPassword() {
+        String input = "PASSwOrd";
+        Boolean output;
+        Boolean expected = false;
+
+        MainActivity mainac = new MainActivity();
+        output = mainac.isntPassword(input);
+        assertEquals(expected, output);
     }
 }
